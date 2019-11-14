1952b66f4e9bbbbb3d190b23682c451765e8dab3
iths-suriyan-schock <54362647+iths-suriyan-schock@users.noreply.github.com>
2019-11-14 14:50:28 +0100

    Improvements

---
+++ js/main.js
@@ -0,0 +1,114 @@
+let cards = `[{"name":"Earthshaker Giant","imageURL":"https://img.scryfall.com/cards/border_crop/front/d/b/dbcdbf7a-9294-47ad-9f93-c16b78c7463a.jpg?1571417205"},{"name":"Fiendish Duo","imageURL":"https://img.scryfall.com/cards/border_crop/front/a/c/acb3ce9b-ee4f-410a-8db3-e87aeb0a4444.jpg?1571417211"},{"name":"Calculating Lich","imageURL":"https://img.scryfall.com/cards/border_crop/front/1/7/17b2ed72-d0f0-4d8d-bb5e-dce08d157466.jpg?1571417182"},{"name":"Sphinx of Enlightenment","imageURL":"https://img.scryfall.com/cards/border_crop/front/0/f/0faa9eea-fbf1-41f7-9def-1ec3d5134a53.jpg?1571417237"},{"name":"Highcliff Felidar","imageURL":"https://img.scryfall.com/cards/border_crop/front/e/c/ecbeac44-9392-4522-8ff5-87079386bd0a.jpg?1571417221"},{"name":"Rarity","imageURL":"https://img.scryfall.com/cards/border_crop/front/9/a/9ad30de4-8299-4252-99f9-866719ec047d.jpg?1570190444"},{"name":"Princess Twilight Sparkle","imageURL":"https://img.scryfall.com/cards/border_crop/front/9/a/9a4e07b3-b095-4335-b636-2d1c34c7db9b.jpg?1570186054"},{"name":"Fabled Passage","imageURL":"https://img.scryfall.com/cards/border_crop/front/5/d/5da290f7-0a2f-4aca-9ec9-0fb1b4014a66.jpg?1571850767"},{"name":"Fabled Passage","imageURL":"https://img.scryfall.com/cards/border_crop/front/6/c/6c40fe14-2016-44e6-a9d4-c13b254969a3.jpg?1571885895"},{"name":"Castle Vantress","imageURL":"https://img.scryfall.com/cards/border_crop/front/3/d/3dfaba45-3385-4344-9fd6-102298a196ef.jpg?1571850775"},{"name":"Castle Vantress","imageURL":"https://img.scryfall.com/cards/border_crop/front/9/2/92e2debb-a07e-43a8-bec9-6ca384b536f9.jpg?1571885899"},{"name":"Castle Locthwain","imageURL":"https://img.scryfall.com/cards/border_crop/front/0/7/070c2cda-1893-434e-9eb4-cb16179e1973.jpg?1571850784"},{"name":"Castle Locthwain","imageURL":"https://img.scryfall.com/cards/border_crop/front/a/a/aad27ff1-ed28-4536-bfdb-5a5de2aa9505.jpg?1571885902"},{"name":"Castle Garenbrig","imageURL":"https://img.scryfall.com/cards/border_crop/front/a/a/aad91a78-2142-4a0b-a02b-f0f26cb2f3ae.jpg?1571850794"},{"name":"Castle Garenbrig","imageURL":"https://img.scryfall.com/cards/border_crop/front/f/b/fbef0b7f-f680-4915-a74c-300502745ea6.jpg?1571885905"},{"name":"Castle Embereth","imageURL":"https://img.scryfall.com/cards/border_crop/front/5/1/51d972cc-b08c-499c-a35e-51d275d5a4aa.jpg?1571850807"},{"name":"Castle Embereth","imageURL":"https://img.scryfall.com/cards/border_crop/front/a/a/aadb386b-0d3a-41cd-96f9-c453979a12df.jpg?1571885908"},{"name":"Castle Ardenvale","imageURL":"https://img.scryfall.com/cards/border_crop/front/b/5/b5c75a87-7a1a-417e-89ac-02608f830a5d.jpg?1571850814"},{"name":"Castle Ardenvale","imageURL":"https://img.scryfall.com/cards/border_crop/front/1/4/14255ada-7b64-497a-b9c4-5e25060ba6f8.jpg?1571885911"},{"name":"Stonecoil Serpent","imageURL":"https://img.scryfall.com/cards/border_crop/front/e/a/ea931fb3-8989-445d-9af9-c6f2d58ed4ee.jpg?1571850820"},{"name":"Stonecoil Serpent","imageURL":"https://img.scryfall.com/cards/border_crop/front/a/4/a47f84f5-56d2-45f7-acb6-7e0fcab31ee1.jpg?1571885915"},{"name":"Sorcerous Spyglass","imageURL":"https://img.scryfall.com/cards/border_crop/front/8/8/88c75247-5e6c-4d4b-be1e-032f76a7c167.jpg?1571850828"},{"name":"Sorcerous Spyglass","imageURL":"https://img.scryfall.com/cards/border_crop/front/5/5/55ca9cf0-96d2-4f22-aec7-d7bca170c99e.jpg?1571885918"},{"name":"Stormfist Crusader","imageURL":"https://img.scryfall.com/cards/border_crop/front/2/b/2b0045cb-38df-46b1-9ab6-9b5b955a09d9.jpg?1571850835"},{"name":"Stormfist Crusader","imageURL":"https://img.scryfall.com/cards/border_crop/front/f/3/f38fb8e3-7155-468d-8e47-6d9c60490ac4.jpg?1571885921"},{"name":"The Royal Scions","imageURL":"https://img.scryfall.com/cards/border_crop/front/4/9/49b32cc7-9f77-48a0-9066-1232a06acff0.jpg?1571850843"},{"name":"The Royal Scions","imageURL":"https://img.scryfall.com/cards/border_crop/front/8/a/8a5d5a7b-43ef-4259-ac38-ec49a51f3476.jpg?1571885925"},{"name":"Outlaws' Merriment","imageURL":"https://img.scryfall.com/cards/border_crop/front/7/e/7ed44b99-c0f3-4bb0-b08b-6d6c24f7fa4a.jpg?1571850852"},{"name":"Outlaws' Merriment","imageURL":"https://img.scryfall.com/cards/border_crop/front/6/d/6da207c0-a83e-4f41-92a5-c40f3aa230bf.jpg?1571885928"},{"name":"Oko, Thief of Crowns","imageURL":"https://img.scryfall.com/cards/border_crop/front/a/c/acbb9670-5e79-4f19-b4bc-afbbe130d122.jpg?1571850859"},{"name":"Oko, Thief of Crowns","imageURL":"https://img.scryfall.com/cards/border_crop/front/0/5/058c60d2-25d1-42bf-9747-715e9ff56e0b.jpg?1572956042"},{"name":"Lochmere Serpent","imageURL":"https://img.scryfall.com/cards/border_crop/front/7/e/7e2f8d03-67a3-4dea-90ad-a39851821722.jpg?1571850867"},{"name":"Lochmere Serpent","imageURL":"https://img.scryfall.com/cards/border_crop/front/6/6/667548ce-4196-4017-bb35-9a817490aed0.jpg?1571885934"},{"name":"Garruk, Cursed Huntsman","imageURL":"https://img.scryfall.com/cards/border_crop/front/6/8/68d3ec62-f33e-473f-bb65-da8166544b81.jpg?1571850876"},{"name":"Garruk, Cursed Huntsman","imageURL":"https://img.scryfall.com/cards/border_crop/front/e/a/ea7fccf1-7872-441b-b4e2-41aaaee23858.jpg?1571885937"},{"name":"Faeburrow Elder","imageURL":"https://img.scryfall.com/cards/border_crop/front/4/6/46a91c7a-ec6d-45ee-9f5e-46717a84321f.jpg?1571850885"},{"name":"Faeburrow Elder","imageURL":"https://img.scryfall.com/cards/border_crop/front/3/1/3169c4bb-7a8b-4411-ad05-9c1b50a5ee44.jpg?1571885940"},{"name":"Escape to the Wilds","imageURL":"https://img.scryfall.com/cards/border_crop/front/b/5/b54c91d9-6601-44df-9e49-2777ce8ecfb5.jpg?1571850891"},{"name":"Escape to the Wilds","imageURL":"https://img.scryfall.com/cards/border_crop/front/b/e/be16a90c-793e-45d8-86a1-6545a40e8666.jpg?1571885944"},{"name":"Doom Foretold","imageURL":"https://img.scryfall.com/cards/border_crop/front/a/d/ad140458-9229-4556-bdeb-18633b6a0eae.jpg?1571850899"}]`
+cards = JSON.parse(cards)
+
+let inputField = document.querySelector("#inputCardName");
+
+inputField.addEventListener("keyup", (event) => {
+        if(event.key == "Enter") {
+            let matchingCardNames = []
+
+            for(let card of cards) {
+                if(card.name.toLowerCase().includes(inputField.value.toLowerCase())) {
+                    matchingCardNames.push(card)
+                }
+                // else{
+                //     filteredList.remove("#cardimg")
+                // }
+            }
+            
+            let filteredList = document.querySelectorAll(".results > li")
+            
+            for(let element of filteredList) {
+                element.remove()
+            }
+            
+            for(let card of matchingCardNames) {
+                let resultList = document.querySelector(".results")
+                let newList = document.createElement("li")
+                newList.innerText = card.name
+                resultList.appendChild(newList)
+
+                    newList.addEventListener("click", function(){
+                    let pic = document.querySelector("#cardimg")
+                    pic.src = card.imageURL         
+                    console.log(card)
+                })
+            }
+            
+        }
+    })
+
+
+// async function render() {
+//     let list = await getData()
+//     let ul = document.querySelector("ul")
+//     let itemPrototype = document.querySelector("li.prototype")
+        
+//     for(let item of list) {
+//         let newItem = itemPrototype.cloneNode(true)
+//         newItem.classList.remove("prototype")
+            
+//         newItem.addEventListener("click", function() {
+//             let pic = document.querySelector("img")
+//             pic.src = item.image_uris.border_crop          
+//             })
+            
+//             newItem.innerText = item.name
+            
+//             ul.append(newItem)
+//         }
+//     }
+    
+//     render()
+
+
+// async function getCardDataByPage(page) {
+//     if(page < 1 || page > 3){
+    //         throw Error("Dumskalle")
+    //     }
+    //     return apiData[page-1]
+    //     // let url = "https://api.scryfall.com/cards?page="+page"
+    //     // let url = `https://api.scryfall.com/cards?page=${page}`
+    //     // let response = await fetch(url)
+    //     // let responseBody = await response.json()
+    //     // return responseBody.results
+    // }
+    // async function findCards(query) {
+        //     let url = `https://api.scryfall.com/cards/search?${inputField}`
+        //     let response = await fetch(url)
+        //     let responseBody = await response.json()
+        //     return responseBody.results
+        // }
+        
+        // getCardDataByPage(1)
+        // getCardDataByPage(2)
+        
+        //     let cardSugg = document.querySelector(".dropdown")
+        
+        //     for(let i = 0; i < getData.length; i++) {
+            //         let letterInput = document.createElement(".dropdown")
+            //         cardSugg.append(letterInput)
+            //         letterInput.innerHTML = getData[i]
+            //     }
+    
+
+//---------------------------------------------------------------------------------------------
+
+// let buttons = document.querySelectorAll(".card-button")
+// for(let button of buttons){
+//     button
+//     .addEventListener("click", function(){
+//         let h1 = document.querySelector(".cardPic .sticky h1")
+//         let img = document.querySelector(".cardPic .sticky img")
+//         let cardIndex = 0
+//         h1.innerText = cards[cardIndex].name
+//         img.src = cards[cardIndex].imageURL
+//     })
+// }
+
+
+// let cardImage = document.createElement("img")
+
+// document.getElementById("cardimg").appendChild(cardImage)
+
+// cardImage.src = cards[2].imageURL
